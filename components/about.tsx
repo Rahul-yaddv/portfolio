5098bbcf699993a9924079971b38b3e0ea90bf2a
rahul yadav <rahul08yaduvanshi@gmail.com>
2024-09-06 10:09:10 +0530

    minor UI changes.

---
+++ components/about.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28 mt-8 flex flex-col items-center gap-2 justify-center"
+
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p>
+        Frontend Developer skilled in JavaScript, React, and Next.js. Experienced in creating
+        dynamic and responsive web applications with a strong focus on user experience and
+        performance optimization. Proficient in modern frontend technologies and best practices,
+        including state management and styling. Adept at working in agile environments,
+        collaborating with cross-functional teams, and delivering high-quality code.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,97 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import nextBlogImg from "@/public/next-blog.png";
+import autoAvenue from "@/public/auto-avenue.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated College",
+    location: "Gurgaon, IN",
+    description:
+      "I graduated from P.G Sec-9 govt. college in gurgaon from a degree in B.C.A. Where i developed my fundamental programming and software development skills in Computer Science.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019-2022",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Techvins Software Pvt.",
+    description:
+      "Here i worked primarily as a front-end developer. Here i get to utilize and polish my front-end development skills in a professional environment.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022-2023",
+  },
+  {
+    title: "Frontend developer",
+    location: "Metaverse ventures.",
+    description:
+      "Contributed as a Frontend Developer to the xMail project, a robust bulk email scheduling and management platform. I contributed by working on api integration from various services while also delivering high-quality user interfaces,advanced interactive calendar and complex data visualization.",
+    icon: React.createElement(FaReact),
+    date: "2023-2024",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Next-Blog",
+    description:
+      "Next-Blog is a full-stack post creation Platform with features like authentication ,database storage in MongoDB and Goggle firebase",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: nextBlogImg,
+    projectUrl: "https://next-blog-fr.vercel.app/",
+  },
+  {
+    title: "Auto-Avenue",
+    description:
+      "Auto-Avenue is a modern auto-vehicles information gathering platform.Users can customize their preference based on various categories.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind"],
+    imageUrl: autoAvenue,
+    projectUrl: "https://auto-avenue-livid.vercel.app/",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript/TS",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Redux",
+  "Express.js",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "NextJs",
+  "REACT-QUERY",
+] as const;
